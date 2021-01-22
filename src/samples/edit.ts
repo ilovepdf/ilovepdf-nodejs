fe8dd8ed76908c2f15fd8aee8633e2d0275186c0
Diego Lao Tebar <diego.lao@ilovepdf.com>
2021-01-22 14:11:24 +0100

    Added editpdf tool sample

---
+++ src/samples/edit.ts
@@ -0,0 +1,36 @@
+import EditTask from '@ilovepdf/ilovepdf-js-core/tasks/edit/EditTask';
+import Text from "@ilovepdf/ilovepdf-js-core/tasks/edit/Text";
+import ILovePDFFile from "@ilovepdf/ilovepdf-js-core/utils/ILovePDFFile";
+import dotenv from 'dotenv';
+import fs from 'fs';
+import ILovePDFApi from "..";
+
+// Load env vars.
+dotenv.config();
+
+const instance = new ILovePDFApi(process.env.PUBLIC_KEY!, process.env.SECRET_KEY!);
+
+const task = instance.newTask('editpdf') as EditTask;
+
+task.start()
+.then(() => {
+    const file = new ILovePDFFile('<FILE_PATH>');
+    return task.addFile(file);
+})
+.then(() => {
+    const textElement = new Text({
+        coordinates: { x: 100, y: 100 },
+        dimensions: { w: 100, h: 100 },
+        text: 'test',
+    });
+    return task.addElement(textElement);
+})
+.then(() => {
+    return task.process();
+})
+.then(() => {
+    return task.download();
+})
+.then((data) => {
+    fs.writeFileSync('path/to/local/file_name.txt', data);
+});
