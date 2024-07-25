2db1bd26c98835c737f9a4685dc08fb3c4bb4705
marc_r <marc.rodriguez@ilovepdf.com>
2024-07-25 09:07:56 +0200

    docs: add pdfocr tool sample

---
+++ src/samples/pdfocr.ts
@@ -0,0 +1,28 @@
+import ILovePDFApi from "..";
+import fs from 'fs';
+import dotenv from 'dotenv';
+import ImagePdfTask from '@ilovepdf/ilovepdf-js-core/tasks/ImagePdfTask';
+import ILovePDFFile from '../ILovePDFFile';
+
+
+// Load env vars.
+dotenv.config();
+
+const instance = new ILovePDFApi(process.env.PUBLIC_KEY!, process.env.SECRET_KEY!);
+
+const task = instance.newTask('pdfocr') as ImagePdfTask;
+
+task.start()
+    .then(() => {
+        const file = new ILovePDFFile('<FILE_PATH>');
+        return task.addFile(file);
+    })
+    .then(() => {
+        return task.process({ ocr_languages: ['eng'] });
+    })
+    .then(() => {
+        return task.download();
+    })
+    .then((data) => {
+        fs.writeFileSync('<FILE_PATH>', data);
+    });
